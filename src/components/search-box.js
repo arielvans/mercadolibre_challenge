ea7734fe85d0b1c55c55d200e63a14b6a43f475b
Ariel Vainstein <ariel.vans@gmail.com>
2018-12-16 20:15:45 -0300

    fix

---
+++ src/components/search-box.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react"
+import {
+    Row,
+    Col,
+    Input,
+    Button,
+    InputGroup,
+    InputGroupAddon
+} from 'reactstrap';
+import logoImg from '../assets/Logo_ML.png';
+import searchIcon from '../assets/ic_Search.png';
+
+
+class SearchBox extends Component {
+    constructor(props) {
+        super(props)
+        this.props = props;
+        this.state = {
+            init: 0
+        }
+        
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Col md={10}>
+                    <Row>
+                        <Col md={1}>
+                            <img src={logoImg} alt="LOGO" />
+                        </Col>
+                        <Col md={9}>
+                            <InputGroup>
+                                <Input placeholder="Nunca dejes de buscar" />
+                                <InputGroupAddon addonType="append">
+                                    <Button style={{backgroundColor: '#EEEEEE', border: 0}}>
+                                        <img src={searchIcon} alt="SEARCH_ICON" />
+                                    </Button>
+                                </InputGroupAddon>
+                            </InputGroup>
+                        </Col>
+                    </Row>
+                </Col>
+            </React.Fragment>
+        );
+    }
+}
+
+export default SearchBox;
